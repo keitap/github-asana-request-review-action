3fa609fc107f7fd029e17c37e8b49fd03e3621b8
Keita Kitamura <keitap@gmail.com>
2021-12-31 21:02:24 +0900

    add conf test
    Signed-off-by: Keita Kitamura <keitap@gmail.com>

--- config_test.go
+++ config_test.go
@@ -9,16 +9,33 @@ import (
 )
 
 func TestLoadConfig(t *testing.T) {
-	data, err := os.ReadFile("./testdata/config.yml")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("config_v1.0.yml", func(t *testing.T) {
+		data, err := os.ReadFile("./testdata/config_v1.0.yml")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	c, err := LoadConfig(data)
-	require.NoError(t, err)
+		c, err := LoadConfig(data)
+		require.NoError(t, err)
 
-	assert.Equal(t, 1, c.DueDate)
-	assert.Equal(t, "123", c.Accounts["user1"])
-	assert.Equal(t, "456", c.Accounts["user2"])
-	assert.Equal(t, "789", c.Accounts["user3"])
+		assert.Equal(t, 1, c.DueDate)
+		assert.Equal(t, "123", c.Accounts["user1"])
+		assert.Equal(t, "456", c.Accounts["user2"])
+		assert.Equal(t, "789", c.Accounts["user3"])
+	})
+
+	t.Run("config_v1.1.yml", func(t *testing.T) {
+		data, err := os.ReadFile("./testdata/config_v1.1.yml")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		c, err := LoadConfig(data)
+		require.NoError(t, err)
+
+		assert.Equal(t, 3, c.DueDate)
+		assert.Equal(t, true, c.Holidays["2021-12-31"])
+		assert.Equal(t, false, c.Holidays["2022-01-01"])
+		assert.Equal(t, false, c.Holidays["2022-01-02"])
+	})
 }
